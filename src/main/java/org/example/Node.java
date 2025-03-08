9a3edc6017fe112de94aab20c18f8103d0dc30d5
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-03-08 16:41:15 +0200

    Add Node

---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Node<E> {
+    E value;
+    Node<E> next;
+    Node<E> prev;
+
+    public Node(E value) {
+        this.value = value;
+    }
+
+    public E getValue() {
+        return value;
+    }
+
+    public void setValue(E value) {
+        this.value = value;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public Node<E> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<E> prev) {
+        this.prev = prev;
+    }
+}
